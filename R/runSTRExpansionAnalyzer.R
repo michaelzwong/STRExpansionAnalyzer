6954dcafa6e16d9ef36a5eb7e33eb313439b6aa2
Michael Wong <michaelzhwong@gmail.com>
2020-12-09 15:47:50 -0800

    Final update for submission

--- R/runSTRExpansionAnalyzer.R
+++ R/runSTRExpansionAnalyzer.R
@@ -9,6 +9,11 @@
 #'
 #' @author Michael Wong, \email{michaelz.wong@mail.utoronto.ca}
 #'
+#' @references
+#' Winston Chang, Joe Cheng, JJ Allaire, Yihui Xie and Jonathan McPherson
+#' (2020). shiny: Web Application Framework for R. R package version 1.5.0.
+#' https://CRAN.R-project.org/package=shiny
+#'
 #' @export
 #' @importFrom shiny runApp
 runSTRExpansionAnalyzer <- function() {
